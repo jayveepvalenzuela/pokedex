6dab21486de239ab9c3d6e6d874892472d7f590f
Jayvee Valenzuela <jayveevalenzuela>
2021-03-16 14:47:19 +0800

    Set container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
+    container: {
+      screens: {
+         md: '1140px',
+      }
+    },
     extend: {},
   },
   variants: {
