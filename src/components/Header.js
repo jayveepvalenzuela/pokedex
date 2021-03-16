b34a19edf4078b77e29748ad5b01fe2237b60846
Jayvee Valenzuela <jayveevalenzuela>
2021-03-16 19:35:42 +0800

    Update header + cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
-import logo from '../logo.webp';
+import logo from '../media/logo.webp';
 
 function Header() {
   return (
-    <header className='sticky top-0 bg-red-600'>
+    <header className='sticky top-0 z-10 shadow-md bg-red-600'>
       <div className='container mx-auto p-5'>
-        <a href='/'><img className='w-6' src={logo} alt='Logo' /></a>
+        <a href='/'>
+          <img className='w-6' src={logo} alt='Logo' />
+        </a>
       </div>
     </header>
   );
