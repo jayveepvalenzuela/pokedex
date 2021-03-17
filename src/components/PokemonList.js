a004d8f8ade195dabbf0bc0d5092ffad8c224986
Jayvee Valenzuela <jayveevalenzuela@outlook.com>
2021-03-17 13:31:55 +0800

    Update card content

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,13 +1,27 @@
+import { useState, useEffect } from 'react';
 import PokemonCard from './PokemonCard';
+import axios from 'axios';
 
 function PokemonList() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => {
+        setPokemonList(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className='container mx-auto px-5'>
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-        <PokemonCard />
+        {pokemonList.map((pokemon, i) => <PokemonCard key={i} name={pokemon.name} index={i + 1} />)}
       </div>
     </div>
-  );
+  )
 }
 
 export default PokemonList;
