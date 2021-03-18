7c93a7491c4e7b137fdb6fff713ef06724389165
Jayvee Valenzuela <jayveevalenzuela@outlook.com>
2021-03-18 17:50:41 +0800

    Convert and serve own sprites

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -12,7 +12,7 @@ function PokemonCard({ name, id }) {
       className='relative transform-gpu shadow rounded p-6 text-center cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg hover:bg-gray-50'
       onClick={() => showInfo(id)}>
       <div className='absolute top-3 right-6 text-sm text-gray-300'>{`#${id}`}</div>
-      <img className='block w-3/4 mx-auto' src={`https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/other/official-artwork/${id}.png?raw=true`} alt='' />
+      <img className='block w-3/4 mx-auto' src={`/pokemon/${id}.webp`} alt='' />
       <h2 className='text-xl font-bold capitalize text-gray-700'>{name}</h2>
     </div>
   );
