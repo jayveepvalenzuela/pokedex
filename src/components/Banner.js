d49c6fca5b2c276dcdf6cc2ea676df2e0788fc45
Jayvee Valenzuela <jayveevalenzuela@outlook.com>
2021-03-23 20:09:44 +0800

    Add loader and banner update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,7 @@
 import bannerImg from '../media/bg-banner.webp';
 
 function Banner() {
-  const bannerStyles = {
-    backgroundImage: `url(${bannerImg})`,
-    backgroundPosition: 'center',
-  }
-
-  return (
-    <div className='mb-12 h-96 bg-gray-100 bg-cover bg-no-repeat' style={bannerStyles}></div>
-  );
+  return <div className='mb-12 h-96 bg-gray-100 bg-cover bg-no-repeat bg-center' style={{ backgroundImage: `url(${bannerImg})` }}></div>;
 }
 
 export default Banner;
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Context } from '../store/store';
 import * as pokemonAPI from '../data/pokemon';
+import loaderImg from '../media/loader.webp';
 
 function PokemonCard({ name, id }) {
   const [state, dispatch] = useContext(Context);
+  const [loadingData, setLoadingData] = useState(false);
 
   function showPokemonModal(id) {
+    setLoadingData(true);
+
     pokemonAPI.getPokemon(id).then(obj => {
       const pokemonData = obj.data;
 
@@ -14,7 +18,10 @@ function PokemonCard({ name, id }) {
 
         dispatch({ type: 'SET_POKEMON_PROFILE', payload: { ...pokemonData, ...pokemonSpecies } });
       }).then(() => {
-        if (!state.modal) dispatch({ type: 'SET_MODAL', payload: true });
+        if (!state.modal) {
+          dispatch({ type: 'SET_MODAL', payload: true });
+          setLoadingData(false);
+        }
       });
     });
   }
@@ -24,7 +31,11 @@ function PokemonCard({ name, id }) {
       className='relative transform-gpu shadow rounded p-6 text-center cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg hover:bg-gray-50'
       onClick={() => showPokemonModal(id)}>
       <div className='absolute top-3 right-6 text-sm text-gray-300'>{`#${id}`}</div>
-      <img className='w-3/4 mx-auto' src={`/pokemon/${id}.webp`} alt='' />
+      {
+        loadingData ?
+          <img className='w-3/4 mx-auto opacity-10 animate-spin invert' src={loaderImg} alt='Loading..' /> :
+          <img className='w-3/4 mx-auto' src={`/pokemon/${id}.webp`} alt='' />
+      }
       <h2 className='text-xl font-bold capitalize text-gray-700'>{name}</h2>
     </div>
   );
