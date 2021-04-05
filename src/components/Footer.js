da0d5a0b45a7fd4da645fc0475fe27d3ed72766f
Jayvee Valenzuela <jayveevalenzuela@outlook.com>
2021-04-05 09:40:00 +0800

    Add small disclaimer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 function Footer() {
   return (
-    <footer className='p-12'></footer>
+    <footer className='p-12 text-center'>
+      <small className='text-xs text-gray-400'>Pokémon © 2002-2021 Pokémon. © 1995-2021 Nintendo/Creatures Inc./GAME FREAK inc. TM, ® and Pokémon character names are trademarks of Nintendo.</small>
+    </footer>
   );
 }
 
