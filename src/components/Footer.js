01296585181ca3eb27492cc0972231342666c3fc
Jayvee Valenzuela <jayveevalenzuela@outlook.com>
2021-04-05 12:25:49 +0800

    Use current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className='p-12 text-center'>
-      <small className='text-xs text-gray-400'>Pokémon © 2002-2021 Pokémon. © 1995-2021 Nintendo/Creatures Inc./GAME FREAK inc. TM, ® and Pokémon character names are trademarks of Nintendo.</small>
+      <small className='text-xs text-gray-400'>Pokémon © 2002-{currentYear} Pokémon. © 1995-{currentYear} Nintendo/Creatures Inc./GAME FREAK inc. TM, ® and Pokémon character names are trademarks of Nintendo.</small>
     </footer>
   );
 }
