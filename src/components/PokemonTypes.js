7cf708352f32bb6cbf0c023665b2ae110cf7ed34
Jayvee Valenzuela <jayveevalenzuela@outlook.com>
2024-08-10 18:11:34 +0800

    Remove overriding background-color

--- src/components/PokemonTypes.js
+++ src/components/PokemonTypes.js
@@ -5,7 +5,7 @@ export default function PokemonTypes({ types, classes = '' }) {
         return (
           <li
             key={i}
-            className={`inline rounded mr-2 px-2 py-1 bg-gray-500 text-white bg-${typeData.type.name} uppercase`}
+            className={`inline rounded mr-2 px-2 py-1 text-white bg-${typeData.type.name} uppercase`}
             style={{ fontSize: '.7rem', letterSpacing: '.25px' }}
           >
             {typeData.type.name}
